4e4767a24aeadd72ba963cda3e38caa18e13ab78
Sven Schatter <sven.schatter@gmail.com>
2018-10-29 13:23:33 +0100

    Add simple "getVersionLatest" example to main, remove helloWorld

--- AMASH-fetcher/app/Main.hs
+++ AMASH-fetcher/app/Main.hs
@@ -1,6 +1,14 @@
 module Main where
 
 import Lib
+import Network.Wreq
+import Control.Lens
+import WreqUtil
+import MarketplaceURIs
 
 main :: IO ()
-main = helloWorld
+main = do
+    r <- get $ MarketplaceURIs.appVersionLatest "de.scandio.confluence.plugins.pocketquery"
+    print $ responseGetBody r
+    print $ responseIsOkay r
+    print $ responseGetStatus r
--- AMASH-fetcher/src/Lib.hs
+++ AMASH-fetcher/src/Lib.hs
@@ -1,11 +1,7 @@
 -- | A Lib module.
-module Lib (helloWorld, square) where
+module Lib (square) where
 
--- | Basic Hello World function.
-helloWorld :: IO ()
-helloWorld = putStrLn "Hello, World!"
-
--- | Calculate the square of a number
+-- | Calculate the square of a number.
 square :: Num a
     => a -- ^ The number
     -> a -- ^ The square
