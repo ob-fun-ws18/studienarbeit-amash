9e6c429dca6d2d75f2a98e154c6cb9cb658aa9bd
Sven Schatter <sven.schatter@gmail.com>
2018-10-29 11:04:53 +0100

    Add Travis configuration (Test, Benchmark, Lint)

--- AMASH-fetcher/src/Lib.hs
+++ AMASH-fetcher/src/Lib.hs
@@ -1,6 +1,12 @@
-module Lib
-    ( someFunc
-    ) where
+-- | A Lib module.
+module Lib (helloWorld, square) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+-- | Basic Hello World function.
+helloWorld :: IO ()
+helloWorld = putStrLn "Hello, World!"
+
+-- | Calculate the square of a number
+square :: Num a
+    => a -- ^ The number
+    -> a -- ^ The square
+square n = n^2
