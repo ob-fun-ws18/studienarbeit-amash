a9400faf3e5d7e8fe9efe38efc12f7aa495750c0
Sven Schatter <sven.schatter@gmail.com>
2018-10-29 20:38:51 +0100

    add aeson testing

--- AMASH-fetcher/app/Main.hs
+++ AMASH-fetcher/app/Main.hs
@@ -7,7 +7,9 @@ import Network.Wreq
 import Control.Lens
 import WreqUtil
 import MarketplaceURIs
-import Data.Aeson.Lens (_String, key)
+import Data.Aeson.Lens
+
+import qualified Data.HashMap.Strict as HM
 
 main :: IO ()
 main = do
@@ -18,3 +20,12 @@ main = do
     print $ responseGetStatus r
     -- print $ r ^. responseBody . key "vendorLinks" . key "privacy" . _String
     putStrLn $ "URI: " ++ uri
+
+    let vendorLinks = HM.toList $ r ^. responseBody . key "vendorLinks" . _Object
+    print vendorLinks
+
+    let link = (snd $ vendorLinks !! 1) ^. _String
+    print link
+
+    l <- get $ pack link
+    print l
