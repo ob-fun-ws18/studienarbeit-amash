d65c0b8dfd1112fe9b366e9102488095e0a53142
Sven Schatter <sven.schatter@gmail.com>
2018-10-29 13:52:43 +0100

    Add "app" marketplace URI

--- AMASH-fetcher/src/MarketplaceURIs.hs
+++ AMASH-fetcher/src/MarketplaceURIs.hs
@@ -1,5 +1,6 @@
 -- | Contains URIs for the Atlassian Marketplace REST API
 module MarketplaceURIs (
+        app,
         appVersions,
         appVersionLatest,
         appVersionByBuildNumber,
@@ -12,26 +13,27 @@ module MarketplaceURIs (
 baseURI :: [Char] -> [Char]
 baseURI path = "https://marketplace.atlassian.com/rest/2" ++ path
 
--- | Provides the Atlassian Marketplace REST base URI.
-appURI :: [Char] -> [Char]
-appURI addonKey = baseURI "/addons/" ++ addonKey
-
 -----------------------------------------------------------------------
 
--- https://developer.atlassian.com/platform/marketplace/rest/#api-addons-addonKey-versions-get
-appVersions :: [Char] -> [Char]
-appVersions addonKey = appURI addonKey ++ "/versions"
+-- | Provides the Atlassian Marketplace REST base URI.
+-- | https://developer.atlassian.com/platform/marketplace/rest/#api-addons-get
+app :: [Char] -> [Char]
+app addonKey = baseURI "/addons/" ++ addonKey
 
--- https://developer.atlassian.com/platform/marketplace/rest/#api-addons-addonKey-versions-latest-get
+-- | https://developer.atlassian.com/platform/marketplace/rest/#api-addons-addonKey-versions-get
+appVersions :: [Char] -> [Char]
+appVersions addonKey = app addonKey ++ "/versions"
+
+-- | https://developer.atlassian.com/platform/marketplace/rest/#api-addons-addonKey-versions-latest-get
 appVersionLatest :: [Char] -> [Char]
 appVersionLatest addonKey = appVersions addonKey ++ "/latest"
 
--- https://developer.atlassian.com/platform/marketplace/rest/#api-addons-addonKey-versions-build-pluginBuildNumber-get
--- Build Number die man aus anderen calls kriegt, also z.B. "3005000020"
+-- | https://developer.atlassian.com/platform/marketplace/rest/#api-addons-addonKey-versions-build-pluginBuildNumber-get
+-- | Build Number die man aus anderen calls kriegt, also z.B. "3005000020"
 appVersionByBuildNumber :: [Char] -> [Char] -> [Char]
 appVersionByBuildNumber addonKey buildNumber = appVersions addonKey ++ "/build/" ++ buildNumber
 
--- https://developer.atlassian.com/platform/marketplace/rest/#api-addons-addonKey-versions-name-name-get
--- Tatsächliche Version also z.B. "3.6.1"
+-- | https://developer.atlassian.com/platform/marketplace/rest/#api-addons-addonKey-versions-name-name-get
+-- | Tatsächliche Version also z.B. "3.6.1"
 appVersionByVersion :: [Char] -> [Char] -> [Char]
 appVersionByVersion addonKey version = appVersions addonKey ++ "/name/" ++ version
