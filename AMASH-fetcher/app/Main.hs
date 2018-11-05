ea706d25979e45bde26a147b569879bb21eb1f98
Sven Schatter <sven.schatter@gmail.com>
2018-11-05 10:22:01 +0100

    move "readConfig" to Lib

--- AMASH-fetcher/app/Main.hs
+++ AMASH-fetcher/app/Main.hs
@@ -2,22 +2,20 @@
 
 module Main where
 
-import Lib
+import Lib (readConfig)
 import Network.Wreq
 import Control.Lens
 import WreqUtil
 import MarketplaceURIs
 import Data.Aeson.Lens
 import Control.Monad (mapM_)
-import System.Environment (getArgs)
 
 import qualified Data.HashMap.Strict as HM
 
 main :: IO ()
-main = do
-    plugins <- readConfig
-    mapM_ getPluginData plugins
+main = readConfig >>= mapM_ getPluginData
 
+-- | Gets and prints the data for a plugin key (e.g. "de.scandio.confluence.plugins.pocketquery")
 getPluginData :: [Char] -> IO ()
 getPluginData plugin = do
     let uri = MarketplaceURIs.app $ plugin
@@ -33,12 +31,3 @@ getPluginData plugin = do
 
     let link = (snd $ vendorLinks !! 1) ^. _String
     print link
-
--- | Read config (plugin keys) either from args or from the file "plugins.config" if there are no args
--- | TODO: Read config from DB instead.
-readConfig :: IO [String]
-readConfig = do
-    args <- getArgs
-    if length args > 0
-    then return args
-    else readFile "plugins.config" >>= return . lines
--- AMASH-fetcher/src/Lib.hs
+++ AMASH-fetcher/src/Lib.hs
@@ -1,8 +1,19 @@
 -- | A Lib module.
-module Lib (square) where
+module Lib (square, readConfig) where
+
+import System.Environment (getArgs)
 
 -- | Calculate the square of a number.
 square :: Num a
     => a -- ^ The number
     -> a -- ^ The square
 square n = n^2
+
+-- | Read config (plugin keys) either from args or from the file "plugins.config" if there are no args
+-- | TODO: Read config from DB instead.
+readConfig :: IO [String] -- ^ List of plugin keys
+readConfig = do
+    args <- getArgs
+    if length args > 0
+    then return args
+    else readFile "plugins.config" >>= return . lines
